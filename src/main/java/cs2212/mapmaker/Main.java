b9da340788eba92cc0994eb9ddc0d0b7d4ee1c14
Connor Cummings <ccummi26@uwo.ca>
2023-02-07 09:55:39 -0500

    Create GUI window with a basic menu bar
    Currently, the light theme is always used, but the code can be easily
    changed to use the dark theme instead. In the future, this should be
    loaded from some kind of user preferences file, but I don't know what
    file format we'll be using for that yet.
    
    A few Java system properties are set on startup to make the application
    work better on macOS. I don't have a Mac to test these, but they should
    make the application use the macOS menu bar and set the window title bar
    appearance.
    
    The application's name (used in the window title and About menu item)
    can also be easily changed because I'm not sure what it should be.

--- src/main/java/cs2212/mapmaker/Main.java
+++ src/main/java/cs2212/mapmaker/Main.java
@@ -1,9 +1,49 @@
 package cs2212.mapmaker;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.SwingUtilities;
+
 public final class Main {
+    public static final String APPLICATION_NAME = "Mapmaker";
+
+    private Main() {}
+
     public static void main(String... args) {
-        System.out.println("Hello, world!");
-        System.out.println("Hello again, world!");
-        System.out.println("Hello again, world!");
+        // TODO: Load this from a user preferences file.
+        boolean darkTheme = false;
+
+        // Set up the FlatLaf theme. This must be done before creating the UI.
+        setPropertiesForMacOs(darkTheme);
+        if (darkTheme) FlatDarkLaf.setup();
+        else FlatLightLaf.setup();
+
+        // Create and show the main window.
+        SwingUtilities.invokeLater(() -> {
+            var window = new MainWindow();
+            window.setVisible(true);
+        });
+    }
+
+    /**
+     * Set some Java system properties that visually integrate the application
+     * with macOS.
+     *
+     * <p>In order for the properties to be correctly recognized by macOS, this
+     * method must be called on the main thread before any Java Swing components
+     * are created.</p>
+     *
+     * @param darkTheme Whether to use a dark theme for window title bars.
+     */
+    private static void setPropertiesForMacOs(boolean darkTheme) {
+        // Use the macOS menu bar instead of creating one inside the window.
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+        // Set the application name that appears in the macOS menu bar.
+        System.setProperty("apple.awt.application.name", APPLICATION_NAME);
+
+        // Tell macOS which theme to use for the window title bar. This cannot
+        // be changed after the UI has been created.
+        var appearance = darkTheme ? "NSAppearanceNameDarkAqua" : "NSAppearanceNameAqua";
+        System.setProperty("apple.awt.application.appearance", appearance);
     }
 }
---
+++ src/main/java/cs2212/mapmaker/MainWindow.java
@@ -0,0 +1,63 @@
+package cs2212.mapmaker;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        super(Main.APPLICATION_NAME);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        setJMenuBar(createMenuBar());
+
+        setPreferredSize(new Dimension(1280, 720));
+        pack();
+    }
+
+    private JMenuBar createMenuBar() {
+        var fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        fileMenu.add(new QuitAction());
+
+        var helpMenu = new JMenu("Help");
+        helpMenu.setMnemonic(KeyEvent.VK_H);
+        helpMenu.add(new AboutAction());
+
+        var menuBar = new JMenuBar();
+        menuBar.add(fileMenu);
+        menuBar.add(helpMenu);
+
+        return menuBar;
+    }
+
+    private class AboutAction extends AbstractAction {
+        public AboutAction() {
+            super("About " + Main.APPLICATION_NAME);
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_A);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            JOptionPane.showMessageDialog(
+                    MainWindow.this,
+                    "Information about the application should go here.",
+                    getName(),
+                    JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    private class QuitAction extends AbstractAction {
+        public QuitAction() {
+            super("Quit");
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_Q);
+            putValue(Action.SHORT_DESCRIPTION, "Quit the application.");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            MainWindow.this.dispose();
+        }
+    }
+}
