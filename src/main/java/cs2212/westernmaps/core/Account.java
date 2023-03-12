5ce620f97ec92153a954360cd0a26819a5fcb4b9
Connor Cummings <ccummi26@uwo.ca>
2023-03-12 16:25:59 -0400

    Make core classes immutable
    The biggest benefit of this change is that it allows us to use Java
    records to implement these classes instead of having to write lots of
    getter/setter soup. This is especially important as previously each
    getter and setter would have required an individual Javadoc comment (per
    the project specification).
    
    Updating information stored in these classes should be accomplished by
    creating a new object with the same values except for what you want to
    change.

--- src/main/java/cs2212/westernmaps/core/Account.java
+++ src/main/java/cs2212/westernmaps/core/Account.java
@@ -1,34 +1,10 @@
 package cs2212.westernmaps.core;
 
-public final class Account {
-    private String username;
-    private byte[] passwordHash;
-    private boolean developer;
-
-    public Account(String username, byte[] passwordHash, boolean developer) {
-        this.username = username;
-        this.passwordHash = passwordHash;
-        this.developer = developer;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public boolean isDeveloper() {
-        return developer;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(char[] password) {
+// Note: Although the content of a byte[] is mutable, please don't mutate the
+//       password hash (it might break things in the future).
+public record Account(String username, byte[] passwordHash, boolean developer) {
+    public Account withPassword(char[] password) {
         throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    public void setDeveloper(boolean developer) {
-        this.developer = developer;
     }
 
     public boolean isPasswordCorrect(char[] password) {
