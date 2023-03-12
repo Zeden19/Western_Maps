3bc6d7aaf25c9e916669b6d29b479ea94ae0921e
Connor Cummings <ccummi26@uwo.ca>
2023-03-12 17:35:25 -0400

    Add Javadoc comments to core classes

--- src/main/java/cs2212/westernmaps/core/POI.java
+++ src/main/java/cs2212/westernmaps/core/POI.java
@@ -1,3 +1,14 @@
 package cs2212.westernmaps.core;
 
+/**
+ * A point of interest (POI) on a map.
+ *
+ * @param name        The display name of this POI.
+ * @param description A long-form description of this POI.
+ * @param x           The X-coordinate of this POI on the map of its floor.
+ * @param y           The Y-coordinate of this POI on the map of its floor.
+ * @param favorite    Whether this POI is one of the user's favorites.
+ * @param floor       The floor this POI is on.
+ * @param layer       The layer this POI is assigned to.
+ */
 public record POI(String name, String description, int x, int y, boolean favorite, Floor floor, Layer layer) {}
