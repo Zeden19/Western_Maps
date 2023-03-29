4ec9efedc9741c38d2a8cecf51782ff3178e46a0
Connor Cummings <ccummi26@uwo.ca>
2023-03-29 16:31:46 -0400

    Rename Database to UndoHistory
    The new name more accurately reflects what the class does.

--- src/main/java/cs2212/westernmaps/core/UndoHistory.java
+++ src/main/java/cs2212/westernmaps/core/UndoHistory.java
@@ -4,40 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A container for the application's data with undo history.
- *
- * <p>Although this class is called {@code Database}, it does not represent an
- * actual database system or a connection to one; it simply holds data.</p>
+ * Manages the application's data and allows undoing and redoing changes.
  */
-public final class Database {
+public final class UndoHistory {
     private final List<DatabaseState> history = new ArrayList<>();
     private int currentStateIndex;
 
     /**
-     * Creates a new database with one state in the history.
+     * Creates a new undo history with one state.
      *
-     * @param initialState The initial state of the database.
+     * @param initialState The initial database state.
      */
-    public Database(DatabaseState initialState) {
+    public UndoHistory(DatabaseState initialState) {
         history.add(initialState);
         currentStateIndex = 0;
     }
 
     /**
-     * Gets the current state of this database.
+     * Gets the current database state for this undo history.
      *
-     * @return The current state of this database.
+     * @return The current state.
      */
     public DatabaseState getCurrentState() {
         return history.get(currentStateIndex);
     }
 
     /**
-     * Modifies the current state of this database.
+     * Adds a new database state to this undo history and makes it current.
      *
-     * <p>The previous state will be added to the undo history. If this method
-     * is called in the past (that is, when redo is possible), then any states
-     * in the future will be overwritten.</p>
+     * <p>If this method is called in the past (that is, when redo is possible),
+     * then any states in the future will be overwritten.</p>
      *
      * @param state The state to make current.
      */
@@ -48,12 +44,12 @@ public final class Database {
     }
 
     /**
-     * Clears the undo history of this database.
+     * Clears this undo history.
      *
      * <p>After this function is called, undo and redo will not be possible. The
      * current state is left intact.</p>
      */
-    public void clearHistory() {
+    public void clear() {
         var currentState = getCurrentState();
         history.clear();
         history.add(currentState);
@@ -61,7 +57,7 @@ public final class Database {
     }
 
     /**
-     * Moves backwards in the undo history, changing the current state.
+     * Moves backwards in this undo history, changing the current state.
      *
      * <p>If the current state is at the beginning of the undo history, then
      * this function will return {@code false} and the current state will not be
@@ -78,7 +74,7 @@ public final class Database {
     }
 
     /**
-     * Moves forwards in the undo history, changing the current state.
+     * Moves forwards in this undo history, changing the current state.
      *
      * <p>If the current state is at the end of the undo history, then this
      * function will return {@code false} and the current state will not be
---
+++ src/test/java/cs2212/westernmaps/core/UndoHistoryTest.java
@@ -0,0 +1,148 @@
+package cs2212.westernmaps.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public final class UndoHistoryTest {
+    private static final byte[] EMPTY_PASSWORD_HASH = new byte[0];
+
+    @Test
+    public void testInitialState() {
+        var history = new UndoHistory(DatabaseState.empty());
+        Assertions.assertEquals(DatabaseState.empty(), history.getCurrentState());
+    }
+
+    @Test
+    public void testPushState() {
+        var state0 = DatabaseState.empty();
+        var history = new UndoHistory(state0);
+        Assertions.assertEquals(state0, history.getCurrentState());
+
+        var asdfAccount = new Account("asdf", EMPTY_PASSWORD_HASH, false);
+        var state1 = history.getCurrentState().modifyAccounts(accounts -> Lists.append(accounts, asdfAccount));
+        history.pushState(state1);
+        Assertions.assertEquals(state1, history.getCurrentState());
+
+        var hjklAccount = new Account("hjkl", EMPTY_PASSWORD_HASH, true);
+        var state2 = history.getCurrentState().modifyAccounts(accounts -> Lists.append(accounts, hjklAccount));
+        history.pushState(state2);
+        Assertions.assertEquals(state2, history.getCurrentState());
+
+        // pushState(...) adds states to the history, so undo is possible.
+        Assertions.assertTrue(history.undo());
+        Assertions.assertEquals(state1, history.getCurrentState());
+        Assertions.assertTrue(history.undo());
+        Assertions.assertEquals(state0, history.getCurrentState());
+
+        // We're at the start, so undo is no longer possible.
+        Assertions.assertFalse(history.undo());
+        Assertions.assertEquals(state0, history.getCurrentState());
+    }
+
+    @Test
+    public void testPushStateInThePast() {
+        var state0 = DatabaseState.empty();
+        var history = new UndoHistory(state0);
+        Assertions.assertEquals(state0, history.getCurrentState());
+
+        // Start by populating the history with some states.
+        var asdfAccount = new Account("asdf", EMPTY_PASSWORD_HASH, false);
+        var state1 = history.getCurrentState().modifyAccounts(accounts -> Lists.append(accounts, asdfAccount));
+        history.pushState(state1);
+        Assertions.assertEquals(state1, history.getCurrentState());
+
+        var hjklAccount = new Account("hjkl", EMPTY_PASSWORD_HASH, true);
+        var state2 = history.getCurrentState().modifyAccounts(accounts -> Lists.append(accounts, hjklAccount));
+        history.pushState(state2);
+        Assertions.assertEquals(state2, history.getCurrentState());
+
+        // Then, undo once...
+        Assertions.assertTrue(history.undo());
+        Assertions.assertEquals(state1, history.getCurrentState());
+
+        // ...and push a state while we're in the past.
+        var qwerAccount = new Account("qwer", EMPTY_PASSWORD_HASH, true);
+        var state3 = history.getCurrentState().modifyAccounts(accounts -> Lists.append(accounts, qwerAccount));
+        history.pushState(state3);
+        Assertions.assertEquals(state3, history.getCurrentState());
+
+        // Now, state2 should be completely gone from the history, since we
+        // overwrote it.
+        Assertions.assertTrue(history.undo());
+        Assertions.assertEquals(state1, history.getCurrentState());
+        Assertions.assertTrue(history.undo());
+        Assertions.assertEquals(state0, history.getCurrentState());
+
+        // We're at the start, so undo is no longer possible.
+        Assertions.assertFalse(history.undo());
+        Assertions.assertEquals(state0, history.getCurrentState());
+    }
+
+    @Test
+    public void testUndoAndRedo() {
+        var state0 = DatabaseState.empty();
+        var history = new UndoHistory(state0);
+        Assertions.assertEquals(state0, history.getCurrentState());
+
+        // Start by populating the history with some states.
+        var asdfAccount = new Account("asdf", EMPTY_PASSWORD_HASH, false);
+        var state1 = history.getCurrentState().modifyAccounts(accounts -> Lists.append(accounts, asdfAccount));
+        history.pushState(state1);
+        Assertions.assertEquals(state1, history.getCurrentState());
+
+        var hjklAccount = new Account("hjkl", EMPTY_PASSWORD_HASH, true);
+        var state2 = history.getCurrentState().modifyAccounts(accounts -> Lists.append(accounts, hjklAccount));
+        history.pushState(state2);
+        Assertions.assertEquals(state2, history.getCurrentState());
+
+        // We haven't undone anything yet, so redo is not possible.
+        Assertions.assertFalse(history.redo());
+        Assertions.assertEquals(state2, history.getCurrentState());
+
+        // Undo all the way to the start.
+        Assertions.assertTrue(history.undo());
+        Assertions.assertEquals(state1, history.getCurrentState());
+        Assertions.assertTrue(history.undo());
+        Assertions.assertEquals(state0, history.getCurrentState());
+
+        // We're at the start, so undo is no longer possible.
+        Assertions.assertFalse(history.undo());
+        Assertions.assertEquals(state0, history.getCurrentState());
+
+        // Redo everything again.
+        Assertions.assertTrue(history.redo());
+        Assertions.assertEquals(state1, history.getCurrentState());
+        Assertions.assertTrue(history.redo());
+        Assertions.assertEquals(state2, history.getCurrentState());
+
+        // We're at the end again, so redo is no longer possible.
+        Assertions.assertFalse(history.redo());
+        Assertions.assertEquals(state2, history.getCurrentState());
+    }
+
+    @Test
+    public void testClear() {
+        var state0 = DatabaseState.empty();
+        var history = new UndoHistory(state0);
+        Assertions.assertEquals(state0, history.getCurrentState());
+
+        // Start by populating the history with some states.
+        var asdfAccount = new Account("asdf", EMPTY_PASSWORD_HASH, false);
+        var state1 = history.getCurrentState().modifyAccounts(accounts -> Lists.append(accounts, asdfAccount));
+        history.pushState(state1);
+        Assertions.assertEquals(state1, history.getCurrentState());
+
+        var hjklAccount = new Account("hjkl", EMPTY_PASSWORD_HASH, true);
+        var state2 = history.getCurrentState().modifyAccounts(accounts -> Lists.append(accounts, hjklAccount));
+        history.pushState(state2);
+        Assertions.assertEquals(state2, history.getCurrentState());
+
+        // Clear the history and make sure the current state is the same.
+        history.clear();
+        Assertions.assertEquals(state2, history.getCurrentState());
+
+        // Now, we shouldn't be able to undo since the history has been cleared.
+        Assertions.assertFalse(history.undo());
+        Assertions.assertEquals(state2, history.getCurrentState());
+    }
+}
