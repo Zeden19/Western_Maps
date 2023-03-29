6867078beb583156aca9e7f4e6aca87117e5ee3b
Connor Cummings <ccummi26@uwo.ca>
2023-03-29 15:51:26 -0400

    Add helper methods for modifying database states
    These methods allow easily creating a new database state based on an
    existing one with some changes.
    
    TODO: Add unit tests for these methods.

--- src/main/java/cs2212/westernmaps/core/DatabaseState.java
+++ src/main/java/cs2212/westernmaps/core/DatabaseState.java
@@ -7,6 +7,7 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * A snapshot of all the application's data at a given point in time.
@@ -23,6 +24,45 @@ public record DatabaseState(List<Account> accounts, List<Building> buildings, Li
         accounts = List.copyOf(accounts);
         buildings = List.copyOf(buildings);
         pois = List.copyOf(pois);
+    }
+
+    /**
+     * Creates a database state with no accounts, buildings, or POIs.
+     *
+     * @return A new database state.
+     */
+    public static DatabaseState empty() {
+        return new DatabaseState(List.of(), List.of(), List.of());
+    }
+
+    /**
+     * Updates the list of accounts in this database state.
+     *
+     * @param operation A function that updates the list of accounts.
+     * @return          A new state containing the updated list of accounts.
+     */
+    public DatabaseState modifyAccounts(Function<List<Account>, List<Account>> operation) {
+        return new DatabaseState(operation.apply(accounts()), buildings(), pois());
+    }
+
+    /**
+     * Updates the list of buildings in this database state.
+     *
+     * @param operation A function that updates the list of buildings.
+     * @return          A new state containing the updated list of buildings.
+     */
+    public DatabaseState modifyBuildings(Function<List<Building>, List<Building>> operation) {
+        return new DatabaseState(accounts(), operation.apply(buildings()), pois());
+    }
+
+    /**
+     * Updates the list of POIs in this database state.
+     *
+     * @param operation A function that updates the list of POIs.
+     * @return          A new state containing the updated list of POIs.
+     */
+    public DatabaseState modifyPOIs(Function<List<POI>, List<POI>> operation) {
+        return new DatabaseState(accounts(), buildings(), operation.apply(pois()));
     }
 
     /**
