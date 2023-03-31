80d3991252602d7901d67f05a860ccec82318573
Connor Cummings <ccummi26@uwo.ca>
2023-03-31 19:18:01 -0400

    Add POI layer icons
    These icons are from the Material Icons filled set (material.io/icons),
    with a white outline added to help them stand out on the map.
    
    The icon file names have been changed as well. Here are the original
    icon names, just in case anyone needs them in the future:
    
    accessibility.svg <- Accessible Forward
    classroom.svg <- Book
    custom.svg <- Person
    eatery.svg <- Restaurant
    lounge.svg <- Chair
    utility.svg <- Construction

--- src/main/java/cs2212/westernmaps/core/Layer.java
+++ src/main/java/cs2212/westernmaps/core/Layer.java
@@ -1,17 +1,30 @@
 package cs2212.westernmaps.core;
 
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import javax.swing.Icon;
+
 /**
  * A layer that a POI can be assigned to.
  *
  * @see POI
  */
 public enum Layer {
-    ACCESSIBILITY,
-    CLASSROOMS,
-    CUSTOM,
-    EATERIES,
-    FITNESS,
-    LOUNGES,
-    MISCELLANEOUS,
-    UTILITIES,
+    ACCESSIBILITY("accessibility"),
+    CLASSROOMS("classroom"),
+    CUSTOM("custom"),
+    EATERIES("eatery"),
+    FITNESS("fitness"),
+    LOUNGES("lounge"),
+    MISCELLANEOUS("miscellaneous"),
+    UTILITIES("utility");
+
+    private final FlatSVGIcon icon;
+
+    Layer(String iconName) {
+        this.icon = new FlatSVGIcon("cs2212/westernmaps/poi-icons/" + iconName + ".svg", Layer.class.getClassLoader());
+    }
+
+    public Icon getIcon() {
+        return icon;
+    }
 }
