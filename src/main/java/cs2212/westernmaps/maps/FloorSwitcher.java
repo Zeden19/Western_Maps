34041c0838cd1da6c161ab68f14733d65944d974
Connor Cummings <ccummi26@uwo.ca>
2023-04-01 19:14:29 -0400

    Implement button selection logic

--- src/main/java/cs2212/westernmaps/maps/FloorSwitcher.java
+++ src/main/java/cs2212/westernmaps/maps/FloorSwitcher.java
@@ -2,13 +2,18 @@ package cs2212.westernmaps.maps;
 
 import cs2212.westernmaps.core.Floor;
 import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.event.ItemEvent;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
+import javax.swing.ButtonGroup;
 import javax.swing.JComponent;
+import javax.swing.JToggleButton;
 
 public final class FloorSwitcher extends JComponent {
-    private final List<Floor> floors;
+    private final List<Consumer<Floor>> floorSwitchListeners = new ArrayList<>();
 
     /**
      * Creates a new floor switcher.
@@ -19,17 +24,43 @@ public final class FloorSwitcher extends JComponent {
      * @param floors The list of floors to switch between.
      */
     public FloorSwitcher(List<Floor> floors) {
-        this.floors = floors;
-
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-        for (var floor : floors) {
-            var floorButton = new JButton(floor.shortName());
-            floorButton.setPreferredSize(new Dimension(48, 48));
-            floorButton.setMinimumSize(floorButton.getPreferredSize());
-            floorButton.setMaximumSize(floorButton.getPreferredSize());
+        var buttonGroup = new ButtonGroup();
+
+        for (int index = 0; index < floors.size(); index++) {
+            var floorButton = new FloorButton(floors.get(index));
+            if (index == 0) {
+                floorButton.setSelected(true);
+            }
 
             add(floorButton, 0);
+            buttonGroup.add(floorButton);
+        }
+    }
+
+    public void addFloorSwitchListener(Consumer<Floor> listener) {
+        floorSwitchListeners.add(listener);
+    }
+
+    private final class FloorButton extends JToggleButton {
+        private static final Dimension SIZE = new Dimension(40, 40);
+
+        public FloorButton(Floor floor) {
+            super(floor.shortName());
+
+            setPreferredSize(SIZE);
+            setMinimumSize(SIZE);
+            setMaximumSize(SIZE);
+
+            // Prevent Java Swing from replacing the content with ...
+            setMargin(new Insets(2, 2, 2, 2));
+
+            addItemListener(e -> {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    floorSwitchListeners.forEach(listener -> listener.accept(floor));
+                }
+            });
         }
     }
 }
