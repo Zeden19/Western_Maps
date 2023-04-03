b2f5c0cb25b424baa426534500e0318b59a710ca
Randeep <rbhall3@uwo.ca>
2023-04-03 01:39:46 -0400

    Add javadoc to the Database.java file

--- src/main/java/cs2212/westernmaps/core/Database.java
+++ src/main/java/cs2212/westernmaps/core/Database.java
@@ -5,19 +5,34 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-// TODO: Add Javadoc.
-
+/**
+ * This class is for the database of the program. It creates the database that is used for the program
+ * with a directory, json file, and its history.
+ * @author
+ */
 public final class Database {
     private final Path directory;
     private final Path jsonFile;
     private final UndoHistory history;
 
+    /**
+     * The constructor for the database
+     * @param directory, the directory being created
+     * @param jsonFile, the json file being used for the directory
+     * @param history, the history of the new database being created
+     */
     private Database(Path directory, Path jsonFile, UndoHistory history) {
         this.directory = directory;
         this.jsonFile = jsonFile;
         this.history = history;
     }
 
+    /**
+     * opens a new directory
+     * @param directory, the new directory that will be opened
+     * @return the new Database with the directory, jsonFile, and its history
+     * @throws IOException when the file is not found
+     */
     public static Database openDirectory(Path directory) throws IOException {
         var jsonFile = directory.resolve("database.json");
 
@@ -30,13 +45,22 @@ public final class Database {
         return new Database(directory, jsonFile, history);
     }
 
+    /**
+     * getter for the history
+     * @return the databases history
+     */
     public UndoHistory getHistory() {
         return history;
     }
 
+    /**
+     *getter for the current state of database
+     * @return the current state of the database
+     */
     public DatabaseState getCurrentState() {
         return getHistory().getCurrentState();
     }
+
 
     public URI resolveFloorMapUri(Floor floor) {
         var mapPath = directory.resolve(floor.mapPath());
@@ -44,8 +68,8 @@ public final class Database {
     }
 
     /**
-     *
-     * @throws IOException when the file is not found
+     *This method saves the database
+     * @throws IOException when the database file is not found
      */
     public void save() throws IOException {
         try (var stream = Files.newOutputStream(jsonFile)) {
