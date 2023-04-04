fb1c7b1ab995ba6687c8ac1efc54b9e4e4653821
RoyalCheungg <rcheun52@uwo.ca>
2023-04-04 01:16:16 -0400

    Implement JUnit tests for List class
    Successfully implemented append, removeIndex, and replaceIndex test class
    
    Tested multiple times successfully

--- src/test/java/cs2212/westernmaps/core/ListsTest.java
+++ src/test/java/cs2212/westernmaps/core/ListsTest.java
@@ -2,6 +2,7 @@ package cs2212.westernmaps.core;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,11 +16,29 @@ class ListsTest {
     void tearDown() {}
 
     @Test
-    void append() {}
+    void append() {
+        List<Integer> list = List.of(1, 2, 3);
+        List<Integer> result = Lists.append(list, 4);
+
+        assertEquals(List.of(1, 2, 3), list);
+        assertEquals(List.of(1, 2, 3, 4), result);
+    }
 
     @Test
-    void removeIndex() {}
+    void removeIndex() {
+        List<Integer> list = List.of(1, 2, 3);
+        List<Integer> result = Lists.removeIndex(list, 1);
+
+        assertEquals(List.of(1, 2, 3), list);
+        assertEquals(List.of(1, 3), result);
+    }
 
     @Test
-    void replaceIndex() {}
+    void replaceIndex() {
+        List<Integer> list = List.of(1, 2, 3);
+        List<Integer> result = Lists.replaceIndex(list, 1, 4);
+
+        assertEquals(List.of(1, 2, 3), list);
+        assertEquals(List.of(1, 4, 3), result);
+    }
 }
